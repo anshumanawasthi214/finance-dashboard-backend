19f899a23dcd003dad57ecf53b08632bf4abc81d
anshumanawasthi214 <anshumanawasthi214@gmail.com>
2026-04-02 21:52:27 +0530

    added some methods in userRepository

--- src/main/java/com/dashboard/finance/repository/UserRepository.java
+++ src/main/java/com/dashboard/finance/repository/UserRepository.java
@@ -6,6 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dashboard.finance.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-	 Optional<User> findByEmail(String email);
+
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
 }
