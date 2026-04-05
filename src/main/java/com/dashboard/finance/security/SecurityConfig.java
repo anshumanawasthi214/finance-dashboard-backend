92e838bd908e853ab9f60ad3cf1f3c2c0696cc48
anshumanawasthi214 <anshumanawasthi214@gmail.com>
2026-04-06 00:37:52 +0530

    added default admin creation class

--- src/main/java/com/dashboard/finance/security/SecurityConfig.java
+++ src/main/java/com/dashboard/finance/security/SecurityConfig.java
@@ -33,6 +33,8 @@ public class SecurityConfig {
                     // Public APIs
                     .requestMatchers(
                             "/auth/**",
+                            "/swagger-ui/**",
+                            "/v3/api-docs/**",
                             "/h2-console/**")
                     .permitAll()
 
