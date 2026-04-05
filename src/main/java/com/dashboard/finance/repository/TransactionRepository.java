9825db4234f70dd25cf41ba6dbd6222a0c7d60f4
anshumanawasthi214 <anshumanawasthi214@gmail.com>
2026-04-06 00:37:09 +0530

    added transaction methods in this repo

--- src/main/java/com/dashboard/finance/repository/TransactionRepository.java
+++ src/main/java/com/dashboard/finance/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.dashboard.finance.repository;
 
 import java.time.LocalDate;
+
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dashboard.finance.model.Transaction;
 import com.dashboard.finance.model.enums.TransactionType;
+
 
 public interface TransactionRepository
 extends JpaRepository<Transaction, Long> {
